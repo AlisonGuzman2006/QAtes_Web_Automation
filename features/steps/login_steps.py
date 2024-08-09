5879da9b9e81212b1c72271233aa7f07eea7356f
Diego-Claros17 <lclarosrocha@gmail.com>
2024-08-09 07:14:21 -0400

    Optimization of test execution time

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -3,45 +3,39 @@ from behave import given, when, then
 from main.ui.component_pages import ComponentPages
 from main.ui.login_page import LoginPage
 
-
 @given('the user is on the Todoist login page')
 def step_given_user_on_todoist_login_page(context):
     context.component_pages = ComponentPages(context.driver)
     context.login_page = LoginPage(context.driver)
     context.driver.get(context.url)
-    time.sleep(10)
+    # No es necesario usar time.sleep(10) porque las esperas explícitas ya están en ComponentPages
 
 @when('the user enters a valid email "{email}"')
 def step_when_user_enters_valid_email(context, email):
     context.component_pages.search_and_fill_by_id(context.login_page.EMAIL_FIELD_SELECTOR, email)
 
-
 @when('the user enters a valid password "{password}"')
 def step_when_user_enters_valid_password(context, password):
     context.component_pages.search_and_fill_by_id(context.login_page.PASSWORD_FIELD_SELECTOR, password)
-
 
 @when('the user clicks on the "Log in" button')
 def step_when_user_clicks_login_button(context):
     context.component_pages.click_button_by_css(context.login_page.LOGIN_BUTTON_SELECTOR)
 
-
 @then('the user should be redirected to the Todoist dashboard')
 def step_then_user_redirected_to_dashboard(context):
-    time.sleep(10)
+    # No se necesita time.sleep(10) ya que la espera está manejada por WebDriverWait en click_button_by_css
     assert f"{context.url}/app" in context.driver.current_url
 
-#Scenario: Unsuccessful login with invalid credentials
+# Scenario: Unsuccessful login with invalid credentials
 
 @when('the user enters an invalid email "{invalid_email}"')
 def step_when_user_enters_invalid_email(context, invalid_email):
     context.component_pages.search_and_fill_by_id(context.login_page.EMAIL_FIELD_SELECTOR, invalid_email)
 
-
 @when('the user enters an invalid password "{invalid_password}"')
 def step_when_user_enters_invalid_password(context, invalid_password):
     context.component_pages.search_and_fill_by_id(context.login_page.PASSWORD_FIELD_SELECTOR, invalid_password)
-
 
 @then('the Todoist error message "{expected_message}" is displayed')
 def step_the_todoist_error_message_is_displayed(context, expected_message):
--- main/ui/component_pages.py
+++ main/ui/component_pages.py
@@ -1,30 +1,27 @@
 from selenium.webdriver.support import expected_conditions as EC
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-
 
 class ComponentPages:
     def __init__(self, driver):
         self.driver = driver
 
     def search_and_fill_by_id(self, id, value):
-        field = self.driver.find_element(By.ID, id)
+        # Aseguramos que el elemento esté visible antes de interactuar con él.
+        field = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, id))
+        )
         field.clear()
         field.send_keys(value)
 
     def click_button_by_css(self, css_selector):
-        login_button = self.driver.find_element(By.CSS_SELECTOR, css_selector)
+        login_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector))
+        )
         login_button.click()
 
-    def get_text_by_class_name(self, class_name, timeout=40):
-        element = WebDriverWait(self.driver, timeout).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, class_name))
-        )
-        return element.text
-
     def get_text_by_class(self, class_selector, timeout=10):
-        element = WebDriverWait(self.driver, timeout).until(
+        message_error = WebDriverWait(self.driver, timeout).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, class_selector))
         )
-        return element.text
+        return message_error.text
