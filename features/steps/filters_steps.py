48c02def9ff6b34b396aed7cadf2663981fba98f
Nicole Muñoz <201604530@est.umss.edu>
2024-08-12 02:15:50 -0400

    refactor filter_steps

--- features/steps/filters_steps.py
+++ features/steps/filters_steps.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 @when('I create a new filter')
-def step_impl(context):
+def creation_new_filter(context):
     context.component_pages.click_button_by_css(context.filters_page.NEW_FILTER_BUTTON_SELECTOR)
     context.component_pages.click_button_by_css(context.filters_page.TRY_IT_BUTTON_SELECTOR)
     context.component_pages.click_button_by_css(context.filters_page.FILTER_ASSIST_BUTTON_SELECTOR)
@@ -12,14 +12,13 @@ def step_impl(context):
 
 
 @when('I added the filter to favorites')
-def step_impl(context):
-    #context.driver.implicitly_wait(5)
+def filter_added_favorites(context):
     context.component_pages.click_button_by_css(context.filters_page.OPTIONS_MENU_SELECTOR)
     context.component_pages.click_button_by_css(context.filters_page.ADD_FAVORITES_SELECTOR)
 
 
 @then('I should see the text "{remove_from_favorites}" inside the filter menu')
-def step_impl(context, remove_from_favorites):
+def remove_favorite_text(context, remove_from_favorites):
     context.component_pages.click_button_by_css(context.filters_page.OPTIONS_MENU_SELECTOR)
     text = context.component_pages.get_text_by_css_selector(context.filters_page.REMOVE_FAVORITES_SELECTOR)
     assert text == remove_from_favorites, "Wrong text"
