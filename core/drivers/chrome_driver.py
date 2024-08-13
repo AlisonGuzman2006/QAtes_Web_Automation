2062ec6cc7b318e27752f2db1e416f6c54c5f1f6
Nicole Muñoz <201604530@est.umss.edu>
2024-08-13 19:46:04 -0400

    adding debugging

--- core/drivers/chrome_driver.py
+++ core/drivers/chrome_driver.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import os
+
 
 def get_chrome_driver():
     chrome_options = Options()
@@ -9,41 +9,9 @@ def get_chrome_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")
 
     service = Service(executable_path="/usr/local/bin/chromedriver")
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
-
-"""
-def get_chrome_driver():
-    # Leer el archivo config.json
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-
-    # Obtener la configuración para Chrome
-    chrome_config = config['browsers']['chrome']
-
-    # Configurar las opciones de Chrome
-    options = webdriver.ChromeOptions()
-
-    # Verificar si se debe iniciar en modo headless
-    if chrome_config.get('headless', False):
-        options.add_argument('--headless')
-
-    # Verificar si se debe maximizar la ventana
-    if chrome_config.get('maxWindows', False):
-        options.add_argument('--start-maximized')
-    else:
-        # Configurar la resolución de la ventana si no se maximiza
-        resolution = chrome_config['resolution']
-        options.add_argument(f'--window-size={resolution["width"]},{resolution["height"]}')
-
-    # Inicializar el WebDriver con las opciones configuradas
-    driver = webdriver.Chrome(options=options)
-
-    # Configurar el tiempo de espera implícito
-    driver.implicitly_wait(chrome_config['timeout'])
-
-    return driver
-"""
