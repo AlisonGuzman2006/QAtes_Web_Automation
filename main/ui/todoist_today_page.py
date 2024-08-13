d38a1b324cbb4ad1cc842eb809b2bac6a5775a3f
AlisonGuzman2006 <201604723@est.umss.edu>
2024-08-13 05:29:49 -0400

    Issies fixes deleting task

--- main/ui/todoist_today_page.py
+++ main/ui/todoist_today_page.py
@@ -1,61 +1,44 @@
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
+from main.ui.component_pages import ComponentPages
 
 class TodoistTodayPage:
     TODAY_DASHBOARD_URL = "https://app.todoist.com/app/today"
     ADD_TASK_BUTTON_DASHBOARD_SELECTOR = 'nav > div:nth-of-type(2) > button[type="button"][aria-disabled="false"]'
     VIEW_PANEL_BUTTON_SELECTOR = 'button[aria-label="View options menu"]'
 
-    TODAY_TASKS_CONTAINER_SELECTOR = 'div[context="[object Object]"]'
+
+    TODAY_TASKS_CONTAINER_SELECTOR = 'div.list_holder div[data-testid="virtuoso-item-list"]'
     TODAY_TASKS_LIST_SELECTOR = 'div[data-item-index]'
-    TODAY_TASK_CONTENT_SELECTOR = 'div[class="task_content"]'
+    TODAY_TASK_CONTENT_SELECTOR = 'div.task_content'
     TASK_MORE_ACTIONS_BUTTON_SELECTOR = 'button[data-testid="more_menu"]'
     DIALOG_DELETE_BTN_SELECTOR = 'div[data-testid="modal-overlay"] div[role="dialog"] button[data-autofocus="true"]'
-
     MENU_ITEM_INBOX_DASHBOARD_SELECTOR = '#todoist_app ul[aria-label="Main filters"]>div[role="button"]'
 
-    TODAY_SECTION_TASK_LIST_SELECTOR = "task_content"
-    TODAY_BOTTON_EDIT_SELECTOR = 'button:nth-child(1)'
-    TODAY_EDIT_TEXT_SELECTOR = 'p'
-    TODAY_SAVE_BTN_SELECTOR = 'button[data-testid="task-editor-submit-button"]'
-    TODAY_MARK_COMPLETE_SELECTOR = 'button[aria-label="Mark task as complete"]'
-
-    OPEN_TASK_CREATED_SELECTOR = 'div[role="alert"][aria-live="polite"] > div:nth-of-type(2) > button[type="button"][aria-disabled="false"]'
-    EDIT_TASK_NAME_SELECTOR = 'div[data-action-hint="task-detail-view-edit"][aria-label="Task name"]'
-    EDIT_TASK_NAME_CONTAINER_SELECTOR = 'div[contenteditable="true"][aria-label="Task name"]'
-    SAVE_BUTTON_SELECTOR = 'button[data-testid="task-editor-submit-button"]'
-    TASK_TITLE_SELECTOR = '.task_content.task-overview-content-large'
-    COMPLETE_CHECKBOX_SELECTOR = 'button[role="checkbox"][data-action-hint="task-detail-view-complete"]'
-    TOAST_TITLE_COMPLETED_SELECTOR = '[data-testid="toasts-container"] > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(1)'
-    MORE_ACTIONS_TASK_SELECTOR = 'button[aria-label="More actions"][aria-haspopup="menu"]'
-    DELETE_BUTTON_SELECTOR = 'div[role="menuitem"][data-destructive="true"]'
-    CONFIRM_DELETE_BUTTON_SELECTOR = 'button[data-autofocus="true"]'
-    TASK_CONTENT_SELECTOR = 'task_content'
-
-    def __init__(self, driver):
+    def __init__(self, driver: ComponentPages):
+        # here is the web driver wait from component pages
         self.driver = driver
 
     def delete_task(self, task_name):
-        task_list = self.driver.find_element(By.CSS_SELECTOR, self.TODAY_TASKS_CONTAINER_SELECTOR)
+        task_list = self.driver.web_driver.until(EC.presence_of_element_located((By.CSS_SELECTOR, self.TODAY_TASKS_CONTAINER_SELECTOR)))
         task_list_items = task_list.find_elements(By.CSS_SELECTOR, self.TODAY_TASKS_LIST_SELECTOR)
         for task_item in task_list_items:
             task_content = task_item.find_element(By.CSS_SELECTOR, self.TODAY_TASK_CONTENT_SELECTOR)
+            print(task_content.text, 'text')
             if task_content.text == task_name:
+                self.driver.action_chains.move_to_element(task_item).perform()
                 more_actions_btn = task_item.find_element(By.CSS_SELECTOR, self.TASK_MORE_ACTIONS_BUTTON_SELECTOR)
                 more_actions_btn.click()
-                time.sleep(5)
+                time.sleep(1)
 
-                delete_option = self.driver.find_element(By.CSS_SELECTOR, self.MORE_ACTIONS_DELETE_OPTION_SELECTOR)
-                delete_option.click()
-                time.sleep(5)
+                self.driver.click_button_by_css(self.MORE_ACTIONS_DELETE_OPTION_SELECTOR)
+                time.sleep(1)
 
-                delete_btn_dialog = self.driver.find_element(By.CSS_SELECTOR, self.DIALOG_DELETE_BTN_SELECTOR)
-                delete_btn_dialog.click()
-
+                self.driver.click_button_by_css(self.DIALOG_DELETE_BTN_SELECTOR)
                 break
-        time.sleep(3)
 
     def navigate_to_inbox_dashboard(self):
         self.driver.find_element(By.CSS_SELECTOR, self.MENU_ITEM_INBOX_DASHBOARD_SELECTOR).click()
--- main/ui/view_panel_page.py
+++ main/ui/view_panel_page.py
@@ -3,23 +3,25 @@ from selenium.webdriver import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import time
+from main.ui.component_pages import ComponentPages
 
 class ViewPanelPage:
     PRIORITY_DROPDOWN_FILTER_SELECTOR = 'span[aria-labelledby="view_menu__priority"]'
+    SORTING_DROPDOWN_FILTER_SELECTOR = 'li[aria-labelledby="view_menu__sort_by"]'
+    ORDER_DROPDOWN_FILTER_SELECTOR = 'li[aria-labelledby="view_menu__order"]'
+    
     SELECTED_PRIORITY_ITEM_FILTER_SELECTOR = 'li[data-value="p1"]'
     VIEW_POPPER_OVERLAY_SELECTOR = 'body > div:nth-child(31)'
     RESET_FILTER_VIEW_PANEL_SELECTOR = 'div[data-testid="popper__overlay"] div[class=" popper"] li[class="menu_item icon_menu_item danger_menu"]'
 
+    ORDER_FILTER_DESCENDING_ITEM_SELECTOR = 'div[data-testid="popper__overlay"] li[data-value="DESC"]'
+    DUE_DATE_FILTER_SORTING_ITEM_SELECTOR = 'div[data-testid="popper__overlay"] li[data-value="DUE_DATE"]'
     DUE_DATE_FILTER_SELECTOR = 'div[data-testid="popper__overlay"] li[aria-labelledby="view_menu__due_date"]'
     DUE_DATE_FILTER_ITEM_SELECTOR = 'div[data-testid="popper__overlay"] li[data-value="NEXT_7_DAYS"]'
 
-    def __init__(self, driver):
+    def __init__(self, driver: ComponentPages):
         self.driver = driver
-        self.EXP_TIMEOUT = 15
 
     def reset_view_panel(self):
-        reset_btn = WebDriverWait(self.driver, self.EXP_TIMEOUT)
-        reset_btn.until(lambda: EC.element_to_be_clickable(By.CSS_SELECTOR, self.RESET_FILTER_VIEW_PANEL_SELECTOR))
-        reset_btn.click()
-        WebDriverWait(self.driver, self.EXP_TIMEOUT).until(lambda: EC.frame_to_be_available_and_switch_to_it()).send_keys(Keys.ESCAPE)
+        self.driver.click_button_by_css(self.RESET_FILTER_VIEW_PANEL_SELECTOR)
+        self.driver.action_chains.send_keys(Keys.ESCAPE).perform()
