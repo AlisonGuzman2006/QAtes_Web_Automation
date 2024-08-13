b3b51bc064ebef298c597055e9264469fb1f885e
Nicole Muñoz <201604530@est.umss.edu>
2024-08-13 19:20:45 -0400

    fix chrome auto installer

--- core/ui/select_browser.py
+++ core/ui/select_browser.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
-from core.drivers.chrome_driver import get_chrome_driver
-from core.drivers.firefox_driver import get_firefox_driver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import chromedriver_autoinstaller
 
 
-def select_browser(browser_name):
-    if browser_name == 'chrome':
-        return get_chrome_driver()
-    elif browser_name == 'firefox':
-        return get_firefox_driver()
-    else:
-        raise ValueError(f"Browser {browser_name} is not supported.")
+def get_chrome_driver():
+    chromedriver_autoinstaller.install()
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver
